638b6fd97ff1f8dd2a75959255c5559264cbeb58
Rahul Sainii <96569692+RAHULSAINI830@users.noreply.github.com>
2024-03-05 17:17:58 +0000

    export

--- my-calculator/next.config.mjs
+++ my-calculator/next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export'};
+const nextConfig = {};
 
-export default nextConfig;
+module.exports = {
+    output: "standalone"
+}
